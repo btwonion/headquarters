7d211b56f0eb589c32ee7c070e61bd57d81766f4
Anton <79146501+btwonion@users.noreply.github.com>
2022-08-09 00:21:36 +0200

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -25,38 +25,10 @@ dependencies {
 
     compileOnly(pluginDep("org.jetbrains.kotlin.plugin.serialization", embeddedKotlinVersion))
     runtimeOnly(pluginDep("org.jetbrains.kotlin.plugin.serialization", kotlinVersion))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0-RC")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
     //Database
     val realmVersion = "1.0.2"
     implementation(pluginDep("io.realm.kotlin", realmVersion))
-    implementation("io.realm.kotlin:library-base:$realmVersion")
-
-    implementation("org.litote.kmongo:kmongo-coroutine:4.6.1")
-
-    //Terminal
-    implementation("com.github.ajalt:mordant:1.2.1")
-    implementation("com.github.ajalt.clikt:clikt:3.5.0")
-
-    //Ktor
-    val ktorVersion = "2.0.3"
-    val logbackVersion = "1.2.10"
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-    implementation("io.ktor:ktor-server-core:$ktorVersion")
-    implementation("io.ktor:ktor-server-websockets:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-websockets:$ktorVersion")
-
-    //Docker
-    val dockerVersion = "3.2.13"
-    implementation("com.github.docker-java:docker-java-transport-httpclient5:$dockerVersion")
-    implementation("com.github.docker-java:docker-java-core:$dockerVersion")
 
     //Fabric
     implementation(pluginDep("fabric-loom", "0.13-SNAPSHOT"))
@@ -66,7 +38,4 @@ dependencies {
     //Paper
     implementation(pluginDep("io.papermc.paperweight.userdev", "1.3.8"))
     implementation(pluginDep("net.minecrell.plugin-yml.bukkit", "0.5.2"))
-
-    //Velocity
-    implementation("com.velocitypowered:velocity-api:3.0.1")
 }
