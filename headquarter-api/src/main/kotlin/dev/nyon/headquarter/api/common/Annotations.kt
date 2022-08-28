9738f10473a5d1763d9da25d1558e1d9f6439d05
Anton <anton.wiblishauser@gmx.de>
2022-08-29 00:30:46 +0200

    create some useful stuff

--- headquarter-api/src/main/kotlin/dev/nyon/headquarter/api/common/Annotations.kt
+++ headquarter-api/src/main/kotlin/dev/nyon/headquarter/api/common/Annotations.kt
@@ -1,22 +1,35 @@
 package dev.nyon.headquarter.api.common
 
 @Retention(value = AnnotationRetention.BINARY)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.PROPERTY, AnnotationTarget.FUNCTION)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.LOCAL_VARIABLE,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
 @RequiresOptIn(
-    message = "This is an internal Headquarter API that " +
-            "should not be used from the outside. No compatibility guarantees are provided. " +
-            "It is recommended to report your use-case of internal API to the Headquarter team.",
+    message = "This is an internal Headquarter API that " + "should not be used from the outside. No compatibility guarantees are provided. " + "It is recommended to report your use-case of internal API to the Headquarter team.",
     level = RequiresOptIn.Level.ERROR
 )
 annotation class InternalHeadquarterAPI
 
 @Retention(value = AnnotationRetention.BINARY)
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.LOCAL_VARIABLE,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
 @RequiresOptIn(
-    message = "This is an experimental API and its use requires care, since" +
-            " stability cannot be guaranteed for this API yet." +
-            " This means that the functionality of this API may not work correctly yet, or that" +
-            " breaking changes can occur at any time.",
+    message = "This is an experimental API and its use requires care, since" + " stability cannot be guaranteed for this API yet." + " This means that the functionality of this API may not work correctly yet, or that" + " breaking changes can occur at any time.",
     level = RequiresOptIn.Level.WARNING
 )
 annotation class ExperimentalHeadquarterApi
--- headquarter-api/src/main/kotlin/dev/nyon/headquarter/api/common/Serializers.kt
+++ headquarter-api/src/main/kotlin/dev/nyon/headquarter/api/common/Serializers.kt
@@ -1,2 +1,23 @@
 package dev.nyon.headquarter.api.common
 
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.util.*
+
+object UUIDSerializer : KSerializer<UUID> {
+
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("uuid", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): UUID {
+        return UUID.fromString(decoder.decodeString())
+    }
+
+    override fun serialize(encoder: Encoder, value: UUID) {
+        encoder.encodeString(value.toString())
+    }
+
+}
