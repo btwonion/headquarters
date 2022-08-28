a73f5f9f55976325c4160c5e9ab11fb232e32fa2
Anton <anton.wiblishauser@gmx.de>
2022-08-29 00:29:56 +0200

    convert distribution class to sealed

--- headquarter-api/src/main/kotlin/dev/nyon/headquarter/api/distribution/Distribution.kt
+++ headquarter-api/src/main/kotlin/dev/nyon/headquarter/api/distribution/Distribution.kt
@@ -1,4 +1,16 @@
 package dev.nyon.headquarter.api.distribution
 
-class Distribution {
+import dev.nyon.headquarter.api.common.InternalHeadquarterAPI
+import dev.nyon.headquarter.api.common.UUIDSerializer
+import kotlinx.serialization.Serializable
+import java.util.*
+
+@InternalHeadquarterAPI
+@Serializable
+sealed class Distribution(val uuid: @Serializable(with = UUIDSerializer::class) UUID) {
+
+    abstract val host: String
+    abstract val availableMemory: Int
+    abstract val usedMemory: Int
+
 }
