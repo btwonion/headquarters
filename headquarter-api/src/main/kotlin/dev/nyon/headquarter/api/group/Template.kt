7d54818d61f31fc04c84b0cb993efbcc2064a6f3
Anton <anton.wiblishauser@gmx.de>
2022-09-01 22:50:05 +0200

    reformat template

--- headquarter-api/src/main/kotlin/dev/nyon/headquarter/api/group/Template.kt
+++ headquarter-api/src/main/kotlin/dev/nyon/headquarter/api/group/Template.kt
@@ -7,5 +7,9 @@ import java.util.*
 
 @Serializable
 data class Template(
-    val uuid: @Serializable(with = UUIDSerializer::class) UUID, var name: String, var host: Host, var path: String, var extraPorts: List<Int>
+    val uuid: @Serializable(with = UUIDSerializer::class) UUID,
+    var name: String,
+    var host: Host,
+    var path: String,
+    var extraPorts: List<Int>
 )
