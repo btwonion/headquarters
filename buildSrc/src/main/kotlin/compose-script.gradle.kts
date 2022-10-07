a67ce5fafe2f9be6c8695ce0cbedf509b6401e26
Anton <anton.wiblishauser@gmx.de>
2022-10-07 18:28:19 +0200

    remove tablericons in favor of feather icons

--- buildSrc/src/main/kotlin/compose-script.gradle.kts
+++ buildSrc/src/main/kotlin/compose-script.gradle.kts
@@ -14,6 +14,6 @@ repositories {
 
 dependencies {
     implementation(compose.desktop.currentOs)
+    @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class) implementation(compose.material3)
     implementation("br.com.devsrsouza.compose.icons.jetbrains:feather:${BuildConstants.composeIconVersion}")
-    implementation("br.com.devsrsouza.compose.icons.jetbrains:tabler-icons:${BuildConstants.composeIconVersion}")
 }
