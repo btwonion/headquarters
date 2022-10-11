9e98b81e21361aee2cb84ace3f876c849d17dc59
Anton <anton.wiblishauser@gmx.de>
2022-10-11 17:05:14 +0200

    finalize local profile creation

--- headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/profile/local/ProfileManager.kt
+++ headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/profile/local/ProfileManager.kt
@@ -3,36 +3,49 @@ package dev.nyon.headquarters.app.profile.local
 import dev.nyon.headquarters.app.connector
 import dev.nyon.headquarters.app.downloadFile
 import dev.nyon.headquarters.app.ktorClient
+import dev.nyon.headquarters.app.profile.models.Config
 import dev.nyon.headquarters.app.profile.models.LocalProfile
 import dev.nyon.headquarters.app.profile.models.Profile
 import dev.nyon.headquarters.app.profile.models.ProjectEntry
 import dev.nyon.headquarters.app.runningDir
-import dev.nyon.headquarters.connector.modrinth.models.project.ProjectType
 import dev.nyon.headquarters.connector.modrinth.models.project.version.Loader
 import dev.nyon.headquarters.connector.modrinth.models.project.version.Version
 import dev.nyon.headquarters.connector.modrinth.requests.getVersions
 import io.ktor.http.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.nio.file.Path
 import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
+import kotlin.io.path.writeBytes
 
 val testModProfile = Profile.ModProfile(
     "1.19.2", Loader.Fabric, listOf(), listOf(), listOf(ProjectEntry("lg17V3i3", "qak00xay")), "sadawa"
 )
 
 suspend fun createProfile(profile: LocalProfile) {
+    realm.write {
+        copyToRealm(profile)
+    }
     val profileDir = Path("${runningDir.toAbsolutePath()}/profiles/${profile.name}/").createDirectories()
 
-    val modVersions = connector.getVersions(testModProfile.mods.map { it.versionID }) ?: error("asd")
-    val modPath = Path("${profileDir.toAbsolutePath()}/mods/").createDirectories()
-    modPath.downloadProjects(modVersions)
+    connector.getVersions(testModProfile.mods.map { it.versionID }).let {
+        if (it == null) return@let
+        Path("${profileDir.toAbsolutePath()}/mods/").createDirectories().downloadProjects(it)
+    }
+
+    testModProfile.configs.forEach {
+        withContext(Dispatchers.IO) {
+            Path("${profileDir.toAbsolutePath()}/config/${if (it.directory != "") "${it.directory}/${it.name}" else it.name}").createDirectories()
+                .writeBytes(it.content)
+        }
+    }
 }
 
 private suspend fun Path.downloadProjects(projects: List<Version>) {
     projects.forEach {
         ktorClient.downloadFile(
-            Url(it.files[0].url),
-            Path("${this@downloadProjects.toAbsolutePath()}/${it.files[0].fileName}")
+            Url(it.files[0].url), Path("${this@downloadProjects.toAbsolutePath()}/${it.files[0].fileName}")
         )
     }
 }
--- headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/profile/models/Config.kt
+++ headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/profile/models/Config.kt
@@ -3,4 +3,4 @@ package dev.nyon.headquarters.app.profile.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Config(val directory: String?, val content: String, val name: String)
+data class Config(val directory: String = "", val content: ByteArray, val name: String)
