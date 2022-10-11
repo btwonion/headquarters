4f88236c05778d9ee17ead953d2823c7076a36dd
Anton <anton.wiblishauser@gmx.de>
2022-10-11 17:04:10 +0200

    improve downloadFile

--- headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/App.kt
+++ headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/App.kt
@@ -12,6 +12,8 @@ import io.ktor.client.utils.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.utils.io.core.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import java.nio.file.Files.write
 import java.nio.file.Path
@@ -38,7 +40,7 @@ suspend inline fun HttpClient.downloadFile(
     url: Url,
     path: Path,
     noinline downloadProgress: (suspend (Double) -> Unit)? = null,
-    builder: HttpRequestBuilder.() -> Unit = {},
+    crossinline builder: HttpRequestBuilder.() -> Unit = {},
 ) = prepareRequest(url) {
     builder(this)
 
@@ -51,21 +53,23 @@ suspend inline fun HttpClient.downloadFile(
         }
     }
 }.execute { response ->
-    val channel = response.bodyAsChannel()
+    withContext(Dispatchers.IO) {
+        val channel = response.bodyAsChannel()
 
-    val partPath = path.parent!!.resolve(path.name + ".part")
+        val partPath = path.parent!!.resolve(path.name + ".part")
 
-    try {
-        while (!channel.isClosedForRead) {
-            val packet = channel.readRemaining(DEFAULT_HTTP_BUFFER_SIZE.toLong())
-            while (!packet.isEmpty) {
-                write(partPath, packet.readBytes())
+        try {
+            while (!channel.isClosedForRead) {
+                val packet = channel.readRemaining(DEFAULT_HTTP_BUFFER_SIZE.toLong())
+                while (!packet.isEmpty) {
+                    write(partPath, packet.readBytes())
+                }
             }
-        }
-        partPath.moveTo(path, overwrite = true)
+            partPath.moveTo(path, overwrite = true)
 
-        downloadProgress?.invoke(1.0)
-    } finally {
-        partPath.deleteIfExists()
+            downloadProgress?.invoke(1.0)
+        } finally {
+            partPath.deleteIfExists()
+        }
     }
 }
