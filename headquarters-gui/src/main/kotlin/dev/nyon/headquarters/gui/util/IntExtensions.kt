130326c90487a668a78cd2ac34d07373c21a4926
Anton <anton.wiblishauser@gmx.de>
2022-10-16 00:22:19 +0200

    add util to create a pretty instant distance string

--- headquarters-gui/src/main/kotlin/dev/nyon/headquarters/gui/util/IntExtensions.kt
+++ headquarters-gui/src/main/kotlin/dev/nyon/headquarters/gui/util/IntExtensions.kt
@@ -1,7 +1,26 @@
 package dev.nyon.headquarters.gui.util
 
+import kotlin.time.Duration
+
 fun Int.toPrettyString(): String {
     if (this >= 1000000) return "${this.toString().dropLast(6)} M"
     if (this >= 1000) return "${this.toString().dropLast(3)} K"
     return this.toString()
 }
+
+fun Duration.distance(): String = if (this.inWholeDays <= -365) "${
+    (this.inWholeDays / 365).toInt().toString().removePrefix("-")
+} years ago"
+else if (this.inWholeDays <= -30) "${
+    (this.inWholeDays / 30).toInt().toString().removePrefix("-")
+} months ago"
+else if (this.inWholeDays <= -1) "${
+    this.inWholeDays.toString().removePrefix("-")
+} days ago"
+else if (this.inWholeHours <= -1) "${
+    this.inWholeHours.toString().removePrefix("-")
+} hours ago"
+else if (this.inWholeMinutes <= -1) "${
+    this.inWholeMinutes.toString().removePrefix("-")
+} minutes ago"
+else "${this.inWholeSeconds.toString().removePrefix("-")} seconds ago"
