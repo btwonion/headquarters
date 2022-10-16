796f5f911478d3490979b214e8451150d86d744a
Anton <anton.wiblishauser@gmx.de>
2022-10-16 22:18:15 +0200

    add versiontype color

---
+++ headquarters-gui/src/main/kotlin/dev/nyon/headquarters/gui/util/VersionExtensions.kt
@@ -0,0 +1,11 @@
+package dev.nyon.headquarters.gui.util
+
+import androidx.compose.ui.graphics.Color
+import dev.nyon.headquarters.connector.modrinth.models.project.version.VersionType
+
+val VersionType.color
+    get() = when (this) {
+        VersionType.Alpha -> Color(0x99db3162)
+        VersionType.Beta -> Color(0x99f7bb43)
+        VersionType.Release -> Color(0x9924a54e)
+    }
