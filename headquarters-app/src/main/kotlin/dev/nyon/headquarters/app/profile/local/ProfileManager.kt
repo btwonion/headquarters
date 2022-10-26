6918c603539e153361d8e98ef0a0756270ffb09c
Anton <anton.wiblishauser@gmx.de>
2022-10-26 20:56:38 +0200

    download fabric libraries on profile creation

--- headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/profile/local/ProfileManager.kt
+++ headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/profile/local/ProfileManager.kt
@@ -1,12 +1,12 @@
 package dev.nyon.headquarters.app.profile.local
 
-import dev.nyon.headquarters.app.connector
-import dev.nyon.headquarters.app.downloadFile
-import dev.nyon.headquarters.app.ktorClient
+import dev.nyon.headquarters.app.*
 import dev.nyon.headquarters.app.profile.models.LocalProfile
 import dev.nyon.headquarters.app.profile.models.Profile
 import dev.nyon.headquarters.app.profile.models.ProjectEntry
-import dev.nyon.headquarters.app.runningDir
+import dev.nyon.headquarters.connector.fabric.models.LauncherMeta
+import dev.nyon.headquarters.connector.fabric.requests.getLoaderOfGameAndLoaderVersion
+import dev.nyon.headquarters.connector.fabric.requests.getLoaderVersions
 import dev.nyon.headquarters.connector.modrinth.models.project.version.Loader
 import dev.nyon.headquarters.connector.modrinth.models.project.version.Version
 import dev.nyon.headquarters.connector.modrinth.requests.getVersions
@@ -14,7 +14,6 @@ import io.ktor.http.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.nio.file.Path
-import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
 import kotlin.io.path.writeBytes
 
@@ -26,17 +25,32 @@ suspend fun createProfile(profile: LocalProfile) {
     realm.write {
         copyToRealm(profile)
     }
-    val profileDir = Path("${runningDir.toAbsolutePath()}/profiles/${profile.name}/").createDirectories()
+    val profileDir = runningDir.resolve("profiles/${profile.name}/").createDirectories()
 
-    connector.getVersions(testModProfile.mods.map { it.versionID }).let {
+    if (testModProfile.loader == Loader.Fabric) {
+        val librariesDir = profileDir.resolve("libraries/").createDirectories()
+        val loaderVersion = fabricConnector.getLoaderOfGameAndLoaderVersion(
+            testModProfile.gameVersion, fabricConnector.getLoaderVersions()!![0].version
+        ) ?: error("loader cannot be found")
+        (loaderVersion.launcherMeta as LauncherMeta).libraries.common.forEach { artifact ->
+            val split = artifact.name.split(":")
+            val fileName = "${split[1]}-${split[2]}.jar"
+            val url = "${artifact.url}${split[0].replace(".", "/")}${
+                split.toMutableList().also { it.removeFirst() }.joinToString("/", prefix = "/", postfix = "/")
+            }$fileName"
+            ktorClient.downloadFile(Url(url), librariesDir.resolve(fileName))
+        }
+    }
+
+    modrinthConnector.getVersions(testModProfile.mods.map { it.versionID }).let {
         if (it == null) return@let
-        Path("${profileDir.toAbsolutePath()}/mods/").createDirectories().downloadProjects(it)
+        profileDir.resolve("mods/").createDirectories().downloadProjects(it)
     }
 
     testModProfile.configs.forEach {
         withContext(Dispatchers.IO) {
-            Path("${profileDir.toAbsolutePath()}/config/${if (it.directory != "") "${it.directory}/${it.name}" else it.name}").createDirectories()
-                .writeBytes(it.content)
+            profileDir.resolve("config/${if (it.directory != "") "${it.directory}/${it.name}" else it.name}")
+                .createDirectories().writeBytes(it.content)
         }
     }
 }
@@ -44,7 +58,7 @@ suspend fun createProfile(profile: LocalProfile) {
 private suspend fun Path.downloadProjects(projects: List<Version>) {
     projects.forEach {
         ktorClient.downloadFile(
-            Url(it.files[0].url), Path("${this@downloadProjects.toAbsolutePath()}/${it.files[0].fileName}")
+            Url(it.files[0].url), this@downloadProjects.resolve(it.files[0].fileName)
         )
     }
 }
