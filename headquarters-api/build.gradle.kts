dfc395d2c6ad3ca21e5a35dd3b590698d2b0898e
Anton <anton.wiblishauser@gmx.de>
2023-01-06 16:05:53 +0100

    add api publishing

--- headquarters-api/build.gradle.kts
+++ headquarters-api/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm")
     `kotlin-project-script`
+    `maven-publish`
+    signing
 }
 
 repositories {
@@ -12,4 +14,30 @@ dependencies {
     implementation("dev.nyon:headquarters-connector-core:${BuildConstants.connectorVersion}")
     implementation("dev.nyon:headquarters-connector-fabric:${BuildConstants.connectorVersion}")
     implementation("dev.nyon:headquarters-connector-mojang:${BuildConstants.connectorVersion}")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "nyon"
+            url = uri("https://repo.nyon.dev/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.nyon"
+            artifactId = "headquarters-api"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
