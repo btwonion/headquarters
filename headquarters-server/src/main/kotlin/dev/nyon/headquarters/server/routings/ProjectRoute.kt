f43fc78b3cbe21e8bd857900d19ffcb878c1e292
Anton <anton.wiblishauser@gmx.de>
2023-01-11 16:19:14 +0100

    use new id generator for project id

--- headquarters-server/src/main/kotlin/dev/nyon/headquarters/server/routings/ProjectRoute.kt
+++ headquarters-server/src/main/kotlin/dev/nyon/headquarters/server/routings/ProjectRoute.kt
@@ -3,9 +3,10 @@ package dev.nyon.headquarters.server.routings
 import dev.nyon.headquarters.api.Profile
 import dev.nyon.headquarters.api.Visibility
 import dev.nyon.headquarters.server.database.profiles
-import dev.nyon.headquarters.server.util.generateProfileID
+import dev.nyon.headquarters.server.util.generateAndCheckID
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -23,22 +24,24 @@ fun Routing.configureProfileRoute() {
             else call.respond(profile)
         }
 
-        delete("{id}") {
-            val id = call.parameters["id"]!!
+        authenticate("auth-oauth-github") {
+            delete("{id}") {
+                val id = call.parameters["id"]!!
 
-            profiles.deleteOne(Profile::id eq id)
-            call.respondText("Project removed successfully")
-        }
+                profiles.deleteOne(Profile::id eq id)
+                call.respondText("Project removed successfully")
+            }
 
-        post {
-            val profile = call.receive<Profile>()
+            post {
+                val profile = call.receive<Profile>()
 
-            if (profiles.findOne(Profile::id eq profile.id) != null) call.respond(HttpStatusCode.Conflict)
-            else {
-                launch {
-                    profile.id = generateProfileID(8)
-                    profiles.insertOne(profile)
-                    call.respond(profile)
+                if (profiles.findOne(Profile::id eq profile.id) != null) call.respond(HttpStatusCode.Conflict)
+                else {
+                    launch {
+                        profile.id = generateAndCheckID(8, profiles)
+                        profiles.insertOne(profile)
+                        call.respond(profile)
+                    }
                 }
             }
         }
