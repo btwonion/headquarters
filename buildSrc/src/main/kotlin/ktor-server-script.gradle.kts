c40027746f8d91b1e40f1abaf2670e57a59d11ad
Anton <anton.wiblishauser@gmx.de>
2023-03-02 19:24:02 +0100

    use netty instead of cio server

--- buildSrc/src/main/kotlin/ktor-server-script.gradle.kts
+++ buildSrc/src/main/kotlin/ktor-server-script.gradle.kts
@@ -12,11 +12,11 @@ dependencies {
     val ktorVersion = BuildConstants.ktorVersion
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-cio:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-sessions-jvm:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:${BuildConstants.logbackVersion}")
 }
