5f9e32ffcc30b996733b143d89ead073ee054362
Anton <anton.wiblishauser@gmx.de>
2023-03-18 23:12:25 +0100

    create file check

--- headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/profile/ProfileManager.kt
+++ headquarters-app/src/main/kotlin/dev/nyon/headquarters/app/profile/ProfileManager.kt
@@ -16,9 +16,7 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import java.nio.file.Path
-import kotlin.io.path.createDirectories
-import kotlin.io.path.exists
-import kotlin.io.path.notExists
+import kotlin.io.path.*
 
 
 suspend fun Profile.init() {
@@ -26,34 +24,11 @@ suspend fun Profile.init() {
     val resourcePackDir = profileDir.resolve("resourcepacks/").createDirectories()
     profileDir.resolve("versions/").createDirectories()
 
-    downloadMojangLibraries()
-    downloadLauncherLibraries()
+    assureMojangLibraries()
+    assureLauncherLibraries()
 
     modsDir.downloadProjects(mods)
     resourcePackDir.downloadProjects(resourcePacks)
-
-    val release =
-        requestReleases("adoptium/temurin17-binaries").first { it.name.startsWith("jdk-${minecraftVersion.javaVersion.majorVersion}") }
-    val asset = release.assets.find { asset ->
-        val name = asset.name.dropWhile { it != '-' }.drop(1)
-        name.startsWith("jdk_${arch}_${os.name.lowercase()}_hotspot_") && name.endsWith(os.commonFileEnding)
-    } ?: error("cannot find java package matching your system")
-    val filePath = javaVersionsDir.resolve("java_${minecraftVersion.javaVersion.majorVersion}${os.commonFileEnding}")
-    ktorClient.downloadFile(Url(asset.browser_download_url), filePath)
-    os.commonArchiver.extract(
-        filePath.toFile(),
-        javaVersionsDir.resolve("java_${minecraftVersion.javaVersion.majorVersion}").toFile()
-    )
-
-    val assetIndex = assetsDir.resolve("indexes/${minecraftVersion.assetIndex.id}.json")
-    if (assetIndex.notExists()) {
-        ktorClient.downloadFile(
-            Url(
-                minecraftVersion.assetIndex.url
-            ), assetIndex
-        )
-        downloadAssets()
-    }
 }
 
 private suspend fun Path.downloadProjects(projects: List<Project>) {
@@ -67,7 +42,7 @@ private suspend fun Path.downloadProjects(projects: List<Project>) {
     }
 }
 
-private suspend fun Profile.downloadMojangLibraries() {
+suspend fun Profile.assureMojangLibraries() {
     val libraries =
         minecraftVersion.libraries.filter {
             if (it.rules == null) return@filter true
@@ -85,14 +60,39 @@ private suspend fun Profile.downloadMojangLibraries() {
     ktorClient.downloadFile(Url(minecraftVersion.downloads.client.url), profileDir.resolve("client.jar"))
 }
 
-private suspend fun Profile.downloadLauncherLibraries() {
+suspend fun Profile.assureLauncherLibraries() {
     when (loader) {
-        Loader.Fabric -> FabricCreateProcess(profileDir, this.minecraftVersion, loaderProfile).installLibraries()
+        Loader.Fabric, Loader.Quilt -> FabricCreateProcess(
+            profileDir,
+            this.minecraftVersion,
+            loaderProfile
+        ).installLibraries()
+
         else -> VanillaCreateProcess(profileDir, this.minecraftVersion).installLibraries()
     }
 }
 
-private suspend fun Profile.downloadAssets() {
+suspend fun Profile.assureAssets() {
+    val assetIndex = assetsDir.resolve("indexes/${minecraftVersion.assetIndex.id}.json")
+    if (assetIndex.notExists()) {
+        ktorClient.downloadFile(Url(minecraftVersion.assetIndex.url), assetIndex)
+        assureAssets()
+    }
+    val logFilePath =
+        profileDir.resolve(minecraftVersion.logging.client!!.file.url.takeLastWhile { it != '/' })
+    if (logFilePath.notExists())
+        ktorClient.downloadFile(Url(minecraftVersion.logging.client!!.file.url), logFilePath)
+    var logConfig = logFilePath.readText()
+    logConfig = logConfig.replace(
+        "\"logs/latest.log\"",
+        "\"${profileDir.resolve("logs/latest.log").absolutePathString()}\""
+    )
+    logConfig = logConfig.replace(
+        "\"logs/%d{yyyy-MM-dd}-%i.log.gz\"",
+        "\"${profileDir.resolve("logs/").absolutePathString()}/%d{yyyy-MM-dd}-%i.log.gz\""
+    )
+    logFilePath.writeText(logConfig)
+
     val objectDir = assetsDir.resolve("objects/")
     val assetString = ktorClient.get(minecraftVersion.assetIndex.url).bodyAsText()
     val assetObject = Json.parseToJsonElement(assetString).jsonObject["objects"]
@@ -105,4 +105,20 @@ private suspend fun Profile.downloadAssets() {
             Url("https://dl.hglabor.de/mirror/assets/1.19.4/objects/${hash?.take(2)}/$hash"), filePath
         )
     }
+}
+
+suspend fun Profile.assureJavaVersion() {
+    if (javaVersionsDir.resolve("java_${minecraftVersion.javaVersion.majorVersion}").exists()) return
+    val release =
+        requestReleases("adoptium/temurin17-binaries").first { it.name.startsWith("jdk-${minecraftVersion.javaVersion.majorVersion}") }
+    val asset = release.assets.find { asset ->
+        val name = asset.name.dropWhile { it != '-' }.drop(1)
+        name.startsWith("jdk_${arch}_${os.name.lowercase()}_hotspot_") && name.endsWith(os.commonFileEnding)
+    } ?: error("cannot find java package matching your system")
+    val filePath = javaVersionsDir.resolve("java_${minecraftVersion.javaVersion.majorVersion}${os.commonFileEnding}")
+    ktorClient.downloadFile(Url(asset.browser_download_url), filePath)
+    os.commonArchiver.extract(
+        filePath.toFile(),
+        javaVersionsDir.resolve("java_${minecraftVersion.javaVersion.majorVersion}").toFile()
+    )
 }
