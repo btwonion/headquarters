11f8ec4d1bbd06b4981576b91d1e19ab09d7db8d
Anton <anton.wiblishauser@gmx.de>
2023-04-04 23:59:25 +0200

    launch applicatin in IO scope

--- headquarters-gui/src/main/kotlin/dev/nyon/headquarters/gui/Application.kt
+++ headquarters-gui/src/main/kotlin/dev/nyon/headquarters/gui/Application.kt
@@ -2,13 +2,14 @@ package dev.nyon.headquarters.gui
 
 import dev.nyon.headquarters.app.initApp
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 lateinit var guiScope: CoroutineScope
 suspend fun main() = coroutineScope {
     guiScope = this
-    launch {
+    launch(Dispatchers.IO) {
         initApp()
         initGui()
     }
